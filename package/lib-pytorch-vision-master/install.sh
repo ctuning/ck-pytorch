fac9d4293168f3f31a983f98ac7de8f60748ee67
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-07-03 16:21:28 +0100

    added simple image classifier via PyTorch

--- package/lib-pytorch-vision-master/install.sh
+++ package/lib-pytorch-vision-master/install.sh
@@ -37,7 +37,7 @@ rm -f tmp-pip-help.tmp
 echo "Downloading and installing deps ..."
 echo ""
 
-${CK_ENV_COMPILER_PYTHON_FILE} -m pip install --ignore-installed numpy pyyaml mkl mkl-include setuptools cmake cffi typing --prefix=${EXTRA_PYTHON_SITE}  ${SYS}
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip install --ignore-installed requests --prefix=${EXTRA_PYTHON_SITE}  ${SYS}
 if [ "${?}" != "0" ] ; then
   echo "Error: installation failed!"
   exit 1
@@ -47,17 +47,20 @@ fi
 MAJOR_PYTHON_VERSION=`${CK_ENV_COMPILER_PYTHON_FILE} -c 'import sys;print(sys.version[0])'`
 
 ######################################################################################
-echo "Cloning PyTorch ..."
+echo "Cloning PyTorch Vision ..."
 echo ""
 
 cd ${INSTALL_DIR}
 git clone --recursive ${PACKAGE_URL}
 
-cd pytorch
+cd vision
 
-export NO_CUDA=${NO_CUDA}
+#mkdir -p ${EXTRA_PYTHON_SITE}
+#mkdir -p ${PACKAGE_LIB_DIR}
 
 ${CK_ENV_COMPILER_PYTHON_FILE} setup.py install --prefix=${EXTRA_PYTHON_SITE}
+
+cp -rf build/lib/* ${PACKAGE_LIB_DIR}
 
 ln -s $PACKAGE_LIB_DIR ${INSTALL_DIR}/lib
 
